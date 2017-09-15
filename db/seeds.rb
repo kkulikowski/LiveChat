b8ffb039f5332bde7707e745f1316171a2a4a374
kkulikowski <myownforge@gmail.com>
2017-09-15 11:47:56 +0200

    Add users seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create default user
+
+password = 'q1w2e3R$'
+1.upto(5) do |i|
+  User.create(
+    email: "user-#{i}@example.com",
+    password: password,
+    password_confirmation: password
+  )
+end
