0a70a9a13a6cff45a637f5dc534984b544d30332
kkulikowski <myownforge@gmail.com>
2017-09-15 14:54:51 +0200

    Add some animations and changing states based on actual conversation state

--- app/assets/javascripts/channels/conversation.js
+++ app/assets/javascripts/channels/conversation.js
@@ -10,13 +10,33 @@ App.conversation = App.cable.subscriptions.create("ConversationChannel", {
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
     var conversation = $('#conversations-list').find("[data-conversation-id='" + data['conversation_id'] + "']");
-    conversation.find('.messages-list').find('ul').append(data['message']);
- 
+  
+    // check if under the data window we pass a partial
+    if (data['window'] !== undefined) {
+      var conversation_visible = conversation.is(':visible');
+  
+      if (conversation_visible) {
+        var messages_visible = (conversation).find('.card-body').is(':visible');
+  
+        if (!messages_visible) {
+          conversation.addClass('card--screaming');
+        }
+        conversation.find('.messages-list').find('ul').append(data['message']);
+      }
+      else {
+        $('#conversations-list').append(data['window']);
+        conversation = $('#conversations-list').find("[data-conversation-id='" + data['conversation_id'] + "']");
+        conversation.find('.card-body').toggle();
+      }
+    }
+    else {
+      conversation.find('ul').append(data['message']);
+    }
+  
     var messages_list = conversation.find('.messages-list');
     var height = messages_list[0].scrollHeight;
     messages_list.scrollTop(height);
   },
- 
   speak: function(message) {
     return this.perform('speak', {
       message: message
