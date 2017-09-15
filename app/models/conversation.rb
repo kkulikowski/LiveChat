0f8efb037114dab2dd42a4c7d776923e6a8d9bba
kkulikowski <myownforge@gmail.com>
2017-09-15 11:56:40 +0200

    Update conversation and user models, binding it together

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,2 +1,13 @@
 class Conversation < ApplicationRecord
+  has_many :messages, dependent: :destroy
+  # we have 2 users in each conversation, we need to recognize them
+  belongs_to :sender, foreign_key: :sender_id, class_name: User
+  belongs_to :recipient, foreign_key: :recipient_id, class_name: User
+
+  validates :sender_id, uniqueness: { scope: :recipient_id }
+
+  # returns opposed user to used user
+  def opposed_user(user)
+    user == recipient ? sender : recipient
+  end
 end
